85aeb4b552a4e0d641c27938e2cd93cfbd10b7b2
Dmitry Barskov <dmitry.barskov@flatstack.com>
2022-04-22 14:42:00 +0200

    Add stub implementation

--- services/fwd-tweet-messages/src/createMessage.js
+++ services/fwd-tweet-messages/src/createMessage.js
@@ -1,1 +1,9 @@
-exports.handler = () => {};
+exports.handler = () => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ message: 'OK' }),
+  };
+};
