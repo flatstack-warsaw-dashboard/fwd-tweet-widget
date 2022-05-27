0976c0a3401e4553063839ccaaf26e7dd749af1a
Dmitry Barskov <dmitry.barskov@flatstack.com>
2022-05-27 19:08:14 +0200

    Fix lambda

--- services/fwd-tweet-messages/src/createMessage.js
+++ services/fwd-tweet-messages/src/createMessage.js
@@ -4,25 +4,48 @@ const region = process.env.REGION;
 const table = process.env.DB_TABLE;
 const db = new AWS.DynamoDB({ region });
 
-const appendMessage = async ({ text, sender }) => {
+const appendMessage = async ({ text, author }) => {
   return await db.putItem({
     TableName: table,
     Item: {
       guid: { S: AWS.util.uuid.v4() },
       created_at: { S: new Date().toISOString() },
       text: { S: text },
-      sender: { S: sender },
+      author: { S: author },
     },
   }).promise();
 };
 
-module.exports.handler = async (_event) => {
-  await appendMessage({ text: "Hello there!", sender: "Dima" });
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ message: 'OK' }),
-  };
+const messageParams = ({ body }) => {
+  const params = JSON.parse(body);
+  if (!params || typeof params !== 'object' || !params.message) {
+    throw new Error('Invalid message params. Body should be a JSON with message key.');
+  }
+  const message = params.message;
+  if (!message.text || !message.author) {
+    throw new Error('Invalid message params. Message text and author are required.');
+  }
+  return { text: message.text, author: message.author };
+};
+
+module.exports.handler = async (event) => {
+  try {
+    await appendMessage(messageParams(event));
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message: 'OK' }),
+    };
+  } catch (e) {
+    return {
+      statusCode: 422,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ error: { message: e.message, event } }),
+    };
+  }
 };
