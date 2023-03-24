755d3e805d3cc4bc1b025f615de60f2473a202f8
Dmitry Barskov <dmitry.barskov64@gmail.com>
2023-03-24 11:27:59 +0100

    Install CDK (#21)
    * Rename services/fwd-* -> src/*
    
    * Install CDK

---
+++ test/tweet-widget.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TweetWidget from '../lib/tweet-widget-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tweet-widget-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TweetWidget.TweetWidgetStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
